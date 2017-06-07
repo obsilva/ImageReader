fe031907da3ba7c5db8b73b45c4e717c829be9bb
WillyamDalessandro <willyambds@gmail.com>
2017-06-07 11:32:51 -0300

    Exporta Audio
    Na função de fala agora tbm gera um arquivo audio de saida e salva no temporario.

--- ImageReader.Domain/Class/Speech.cs
+++ ImageReader.Domain/Class/Speech.cs
@@ -1,4 +1,5 @@
-﻿using System.Speech.Synthesis;
+﻿using System.Speech.AudioFormat;
+using System.Speech.Synthesis;
 
 namespace ImageReader.Domain
 {
@@ -12,9 +13,22 @@ namespace ImageReader.Domain
 			//Configura a saida de audio
 			synth.SetOutputToDefaultAudioDevice();
 
-			//
-			synth.Speak(strTexto.ToString());
-		}
+            // Configura o audio a ser salvo 
+            synth.SetOutputToWaveFile(@"C:\Users\Willyam\AppData\Local\Temp\test.wav",
+              new SpeechAudioFormatInfo(32000, AudioBitsPerSample.Sixteen, AudioChannel.Mono));
+
+            // Cria um  SoundPlayer para dar tocar o arquivo de audio.
+            System.Media.SoundPlayer m_SoundPlayer =
+              new System.Media.SoundPlayer(@"C:\Users\Willyam\AppData\Local\Temp\test.wav");
+
+            // Build a prompt.
+            PromptBuilder builder = new PromptBuilder();
+            builder.AppendText("This is sample output to a WAVE file.");
+
+            //Fala
+            synth.Speak(strTexto.ToString());
+            m_SoundPlayer.Play();
+        }
 
 		public void Pausa()
 		{
@@ -22,5 +36,6 @@ namespace ImageReader.Domain
 			SpeechSynthesizer synth = new SpeechSynthesizer();
 			synth.Pause();
 		}
+
 	}
 }
